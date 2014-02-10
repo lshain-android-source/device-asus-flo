dfbca0501b3beddf0d5447fd538d01b73425b4d6
lshain <lshain.gyh@gmail.com>
2014-02-10 11:55:41 +0800

    add preinstall_apk

--- flo.mk
+++ flo.mk
@@ -5,3 +5,19 @@ PRODUCT_PACKAGES += \
 
 # build version
 BUILD_NUMBER := T1.00
+
+
+# copy all files
+define all-preinstall-files-under
+$(patsubst ./%,%, \
+    $(shell cd device/asus/flo/preinstall_apk; \
+    find ./ -type f -and -name "*.apk") \
+)
+endef
+
+# copy device/asus/flo/preinstall_apk/*.apk to system/preinstall/
+PREINSTALL_FILES := $(call all-preinstall-files-under)
+ifneq ($(strip $(PREINSTALL_FILES)),) 
+PRODUCT_COPY_FILES += $(foreach files, $(PREINSTALL_FILES), \
+       $(addprefix device/asus/flo/preinstall_apk/, $(files)):$(addprefix system/preinstall/, $(files)))
+endif
